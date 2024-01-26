807bf310db61b21e8935b71687e2cbbdd8d34dfc
Arvinder Wander <awander@microsoft.com>
2024-01-26 15:23:58 -0800

    fix typo of GB

--- repartition/partition.sh
+++ repartition/partition.sh
@@ -10,7 +10,7 @@ shrink_partition() {
     START=${START//[!0-9]/}
     END=${END//[!0-9]/}
     MIDPOINT=$((($START + $END) / 2))
-    ssh root@$NODE_IP "yes 'yes' | parted $DEVICE resizepart 4 $MIDPOINT"
+    ssh root@$NODE_IP "yes 'yes' | parted $DEVICE resizepart 4 $MIDPOINT"GB
     ssh root@$NODE_IP "parted $DEVICE mkpart primary ext4 $MIDPOINT 100%"
     
 }
